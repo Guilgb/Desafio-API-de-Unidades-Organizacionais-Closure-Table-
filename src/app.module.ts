a3d53febca3e40018c71061eff2b357e1962ad59
Guilherme <guilherme.gb@hotmail.com>
2026-01-05 10:28:33 -0300

    feat: integra o winston logger e banco de dados à aplicação

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
-
+import { ConfigModule } from '@nestjs/config';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { DatabaseModule } from '@shared/modules/database/database.module';
+import { WinstonLoggerService } from '@shared/modules/winston/winston-logger.service';
+import { LoggingInterceptor } from '@shared/modules/winston/winston.interceptor';
+import { WinstonModule } from '@shared/modules/winston/winston.module';
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    WinstonModule,
+    DatabaseModule,
+  ],
+  providers: [
+    WinstonLoggerService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ],
 })
 export class AppModule {}
