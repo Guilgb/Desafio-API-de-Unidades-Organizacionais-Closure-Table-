3f905043c7b29c8223b3754529b03c9fc2171e90
Guilherme <guilherme.gb@hotmail.com>
2026-01-05 16:40:59 -0300

    refactor: remover linhas em branco desnecessárias na interface de repositório de grupos

--- src/modules/groups/interfaces/repositories/group.repository.interface.ts
+++ src/modules/groups/interfaces/repositories/group.repository.interface.ts
@@ -3,12 +3,8 @@ import { IGroup } from '../group.interface';
 
 export abstract class GroupsRepositoryInterface {
   abstract createGroup(name: string): Promise<IGroup>;
-
   abstract findGroupById(id: string): Promise<NodeEntity | null>;
-
   abstract findNodeById(id: string): Promise<NodeEntity | null>;
-
   abstract checkCycle(childId: string, parentId: string): Promise<boolean>;
-
   abstract linkGroupToParent(groupId: string, parentId: string): Promise<void>;
 }
